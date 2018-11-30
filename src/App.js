7dbb24709b73f2aace57d841f57e1f93305ea1f8
清水涼太 <4bjj3228.ryota@gmail.com>
2018-12-01 05:49:55 +0900

    ルーティングの設定追加 : react-native-scrollable-tab-viewを導入した。変更の可能性あり

--- src/App.js
+++ src/App.js
@@ -8,26 +8,37 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import ScrollableTabView from 'react-native-scrollable-tab-view';                  //上部のルーティング設定
+import TabNavigator from 'react-native-tab-navigator';
+import ToDoList from './components/ToDoList'
+import {camera} from 'react-native-vector-icons/FontAwesome';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
     'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    'Shake or press menu for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ScrollableTabView tabBarPosition="bottom">
+        <ToDoList tabLabel='Tab #1' style={styles.container} />
+        <Text tabLabel='Tab #2'>favorite</Text>
+        <Text tabLabel='Tab #3'>{camera}</Text>
+
+      </ScrollableTabView>
+
     );
   }
 }
+      //<View style={styles.container}>
+      //  <Text style={styles.welcome}></Text>
+      //  <Text style={styles.instructions}>T</Text>
+      //  <Text style={styles.instructions}>{instructions}</Text>
+      //</View>
 
 const styles = StyleSheet.create({
   container: {
