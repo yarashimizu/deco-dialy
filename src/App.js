253054457f83356bf805c0a9e2599ca97dcd893d
清水涼太 <4bjj3228.ryota@gmail.com>
2018-12-04 16:37:45 +0900

    不要行の削除 & TabBerの文字列をアイコンにする修正方法の確認(未実装) FaceBookのタブを参考にして実装を行う

--- src/App.js
+++ src/App.js
@@ -6,55 +6,38 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import ScrollableTabView from 'react-native-scrollable-tab-view';                  //上部のルーティング設定
-import TabNavigator from 'react-native-tab-navigator';
-import ToDoList from './components/ToDoList'
-import {camera} from 'react-native-vector-icons/FontAwesome';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu for dev menu',
-});
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, ScrollView } from "react-native";
+import ScrollableTabView from "react-native-scrollable-tab-view";
+import ToDoList from "./components/ToDoList";
+import Icon, { camera } from "react-native-vector-icons/FontAwesome";
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <ScrollableTabView tabBarPosition="bottom">
-        <ToDoList tabLabel='Tab #1' style={styles.container} />
-        <Text tabLabel='Tab #2'>favorite</Text>
-        <Text tabLabel='Tab #3'>{camera}</Text>
-
+      <ScrollableTabView
+        style={styles.container}
+        tabBarPosition="overlayBottom"
+        tabBarBackgroundColor="pink"
+        tabBarActiveTextColor="red"
+      >
+        <Icon tabLabel="camera" name="camera" />
+        <ToDoList tabLabel="aaa" style={styles.container} />
+        <Text tabLabel="Tab #2">favorite</Text>
+        <Text tabLabel="Tab #3" />
       </ScrollableTabView>
-
     );
   }
 }
-      //<View style={styles.container}>
-      //  <Text style={styles.welcome}></Text>
-      //  <Text style={styles.instructions}>T</Text>
-      //  <Text style={styles.instructions}>{instructions}</Text>
-      //</View>
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    marginBottom: 20,
+    marginTop: 50
+  }
 });
